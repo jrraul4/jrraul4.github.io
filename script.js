6c7acd70caf42741c530e5027ef280d00e7b1690
Jose Raul <johndoe@example.com>
2023-07-24 17:58:39 -0400

    Add final javascript

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ const linkNavPort = document.querySelector('#linkNav-port');
 const linkNavAbout = document.querySelector('#linkNav-about');
 const linkNavCont = document.querySelector('#linkNav-cont');
 const windowUl = document.querySelector('.links');
+
+function openWindow() {
+  windowUl.classList.add('show');
+}
+
+function closeWindow() {
+  windowUl.classList.remove('show');
+}
+
+closeBtn.addEventListener('click', closeWindow);
+openBtn.addEventListener('click', openWindow);
+linkNavPort.addEventListener('click', closeWindow);
+linkNavAbout.addEventListener('click', closeWindow);
+linkNavCont.addEventListener('click', closeWindow);
