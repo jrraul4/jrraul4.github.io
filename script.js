9d344d827af7945aff60bc9c2c20f845198054b4
Jhonatan <jhonatancaama@gmail.com>
2023-07-24 15:30:08 -0600

    Add js variables

---
+++ script.js
@@ -0,0 +1,6 @@
+const openBtn = document.querySelector('#openBtn');
+const closeBtn = document.querySelector('#close');
+const linkNavPort = document.querySelector('#linkNav-port');
+const linkNavAbout = document.querySelector('#linkNav-about');
+const linkNavCont = document.querySelector('#linkNav-cont');
+const windowUl = document.querySelector('.links');
