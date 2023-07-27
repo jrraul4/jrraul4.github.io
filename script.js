692d1afecec7bb6d63df88b86fb195ae98bc4ecc
Jhonatan <jhonatancaama@gmail.com>
2023-07-27 09:00:16 -0600

    Add constant cards

--- script.js
+++ script.js
@@ -21,4 +21,16 @@ linkNavAbout.addEventListener('click', closeWindow);
 linkNavCont.addEventListener('click', closeWindow);
 
 /* Projects Section/Generate Cards */
-
+const projects = [
+  {
+    name: 'My Recent Works',
+    other: 'Multi-Post Stories',
+    img: 'img/main-project-img.png',
+    imgPosition: '',
+    shortDescription: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a standard dummy text.",
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s.",
+    technologies: ['CSS', 'HTML', 'Ruby', 'Bootstrap'],
+    liveLink: 'https://jrraul4.github.io',
+    source: 'https://github.com/jrraul4/jrraul4.github.io',
+  },
+];
