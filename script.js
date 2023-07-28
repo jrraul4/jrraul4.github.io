b7399be8a67190f975b4a2d0b2dd23d012449f0e
Jose Raul <johndoe@example.com>
2023-07-28 10:34:42 -0400

    Add code js

--- script.js
+++ script.js
@@ -44,4 +44,89 @@ const projects = [
     liveLink: 'https://jrraul4.github.io',
     source: 'https://github.com/jrraul4/jrraul4.github.io',
   },
+
+  {
+    name: 'Profesional Art Printing Data',
+    img: 'img/project-background-normal.png',
+    imgPosition: '',
+    shortDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s.",
+    technologies: ['HTML', 'Ruby', 'Bootstrap'],
+    liveLink: 'https://jrraul4.github.io',
+    source: 'https://github.com/jrraul4/jrraul4.github.io',
+  },
+
+  {
+    name: 'Profesional Art Printing Data',
+    img: 'img/project-background-normal.png',
+    imgPosition: '',
+    shortDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s.",
+    technologies: ['HTML', 'Ruby', 'Bootstrap'],
+    liveLink: 'https://jrraul4.github.io',
+    source: 'https://github.com/jrraul4/jrraul4.github.io',
+  },
+
+  {
+    name: 'Profesional Art Printing Data',
+    img: 'img/project-background-normal.png',
+    imgPosition: '',
+    shortDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s.",
+    technologies: ['HTML', 'Ruby', 'Bootstrap'],
+    liveLink: 'https://jrraul4.github.io',
+    source: 'https://github.com/jrraul4/jrraul4.github.io',
+  },
+
+  {
+    name: 'Profesional Art Printing Data',
+    img: 'img/project-background-normal.png',
+    imgPosition: '',
+    shortDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s.",
+    technologies: ['HTML', 'Ruby', 'Bootstrap'],
+    liveLink: 'https://jrraul4.github.io',
+    source: 'https://github.com/jrraul4/jrraul4.github.io',
+  },
+
+  {
+    name: 'Profesional Art Printing Data',
+    img: 'img/project-background-normal.png',
+    imgPosition: '',
+    shortDescription: 'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    description: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s.",
+    technologies: ['HTML', 'Ruby', 'Bootstrap'],
+    liveLink: 'https://jrraul4.github.io',
+    source: 'https://github.com/jrraul4/jrraul4.github.io',
+  },
 ];
+
+const cardsContainer = document.getElementById('Portfolio');
+
+let project = projects[index];
+let projectName = project.name;
+let projectOther = project.other;
+let projectDescription = project.shortDescription;
+let projectImage = project.img;
+let projectImagePosition = project.imgPosition;
+let projectLiveLink = project.liveLink;
+let projectSource = project.source;
+
+for (let i = 1; i <= projects.length; i += 1) {
+const cardContent = `
+  <div class="projects${project.imgPosition}">
+    <div class="project-background-normal">
+      <img src="${project.img}" alt="Project number ${i} image">
+    </div>
+    <div class="project-features">
+      <h2>${project.name}</h2>
+      <p class="projects p">${project.shortDescription}</p>
+      <ul class="features">
+        ${project.technologies.map((element) => `<li><a>${element}</a></li>`).join('')}
+      </ul>
+      <button class="button-projects projectButtons">See Project</button>
+    </div>
+  </div>
+`;
+cardsContainer.innerHTML += cardContent;
+}
